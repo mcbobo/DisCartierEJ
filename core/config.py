b78334168ea2a0aad285810ca8582fec5a7737bd
mcbobo <jgb742413096@gmail.com>
2018-10-08 17:59:02 +0800

    1008test

--- core/config.py
+++ core/config.py
@@ -3,10 +3,10 @@
 
 
 # the apk place in container
-APK_NAME = "/apk_shell/xxxx.apk"
+APK_NAME = "/apk_shell/xhs.apk"
 
 # stf_url address
-STF_URL = "http:///xxx.xxx.xxx.xxx:7100/api/v1/devices"
+STF_URL = "http://192.168.10.233:7100/api/v1/devices"
 
 """
 access token of stf
@@ -17,25 +17,25 @@ can be easily generated from the STF UI. Just go to the Settings
 tab and generate a new access token in Keys section.
 Don't forget to save this token somewhere, you will not be able to see it again.
 """
-TOKEN = "xxxxx"
+TOKEN = "41e5f0f70184464d93e787087bdc9bc6bb29076f15c943fc848d577cb1f9f65f"
 
-STF_DELETE_URL = "http://xxx.xxx.xxx.xxx:7100/api/v1/user/devices/"
+STF_DELETE_URL = "http://192.168.10.233:7100/api/v1/user/devices/"
 
 # some variables in desired_capablities
 PLATFORM_NAME = 'Android'
 NEW_COMMAND_TIMEOUT = 60
 
 # come infomation needed by docker_compose.yml
-APPIUM_CARTIEREJ_IMAGE = "xxxxx"
+APPIUM_CARTIEREJ_IMAGE = "appium_cartierej_docker:latest"
 PORTS = 4723
 APPIUM_CARTIEREJ_CMD = "bash /app_shell/app.sh"
-APP_APK_VOLUMES = "/Users/red/xxx:/apk_shell"
+APP_APK_VOLUMES = "/Users/red/apk:/apk_shell"
 
 """
 Use device name as directory to save docker_compose.yml and app.sh
 Need abs path
 """
-DOCKER_COMPOSE_VOLUMES = "xxxx/DisCartierEJ/resources/dockercomposes/"
+DOCKER_COMPOSE_VOLUMES = "/Users/red/DisCartierEJ/resources/dockercomposes/"
 
 # logs save place in local
 LOCAL_LOG_DIR = "/Users/red/tmp/logs/"
